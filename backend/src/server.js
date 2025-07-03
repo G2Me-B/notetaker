165610eb6d6daca7fd41a7d966e2d14ebc58de01
G2Me-B <brutehackenstein@gmail.com>
2025-07-03 11:41:14 +0000

    Update

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,20 +6,24 @@ import path from "path"
 import routes from "./routes/noteRoutes.js"
 import { connectDB } from "./config/db.js"
 import rateLimiter from "./middleware/rateLimiter.js"
+import { allowedNodeEnvironmentFlags } from "process"
 
 // Activate the environment variables
 dotenv.config()
 const PORT = process.env.PORT || 5001
+const __dirname = path.resolve()
 
 // Instantiate the app
 const app = express()
 
 
 // Instantiate a middleware for parsing
-app.use(cors({
-    origin: "http://localhost:5173"
+if (process.env.NODE_ENV !== "production") {
+    app.use(cors({
+        origin: "http://localhost:5173"
+    }
+    ))
 }
-))
 app.use(express.json())
 app.use(rateLimiter)
 
@@ -31,6 +35,14 @@ app.use(rateLimiter)
 
 // Instantiate the routes
 app.use("/api/notes", routes)
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+    })
+}
 
 
 // Activate Database connection
